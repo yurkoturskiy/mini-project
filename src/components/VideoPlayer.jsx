797a6e09777fe0d842899463ffcf6a895c3e92ef
Yurko Turskiy <yurko@overview.ai>
2023-03-15 10:38:38 -0400

    Turn off pinterest

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -8,16 +8,20 @@ export default function VideoPlayer() {
   const [isLoaded, setIsLoaded] = useState(false);
   console.log(isLoaded);
   return (
-    <div className="bg-[#1A1A1C] rounded-2xl overflow-hidden">
+    <motion.div
+      whileHover={{ scale: 1, y: -2 }}
+      className="bg-[#1A1A1C] rounded-2xl overflow-hidden"
+    >
       <Image
         onLoadingComplete={() => setIsLoaded(true)}
         src="/stopframe.png"
+        data-pin-no-hover
         width={384}
         height={685}
         alt="Stopframe"
         data-loaded={isLoaded}
         className={`${styles.image} h-full`}
       />
-    </div>
+    </motion.div>
   );
 }
